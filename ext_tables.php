3daae422bb39399fb18d70ff2f089b5f946a9484
Dima <dmytro.nozdrin.stratis@gmail.com>
2018-12-25 18:45:52 +0200

    [TASK] Remove TCA overrides from ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -1,13 +1,5 @@
 <?php
 defined('TYPO3_MODE') || die;
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    $_EXTKEY, // Note: Vendor name ('Netcreators.') is only added for @see \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin()!
-    'Ncusefulpages',
-    'Useful Pages'
-);
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Rating: Useful Pages');
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_ncusefulpages_domain_model_page');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_ncusefulpages_domain_model_comment');
